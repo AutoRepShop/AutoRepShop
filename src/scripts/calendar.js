5af953af05aa1a484fcb5798a66af20b0fb00864
Unknown <ahristoff@abv.bg>
2018-02-26 11:14:59 +0200

    working on local storage

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,115 +1,155 @@
 // Create the calendar when the document is ready
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    var events_array = [ {
-            title: 'Event 1',
-            // Set to 1st of the month at 12:00 am
-            start: moment().startOf('month'),
-            // Set to en the 1st of the month at 1:30 am
-            end: moment().startOf('month').add(90, 'minutes'),
-            color: 'red'
-        }, {
-            title: 'Event 2',
-            // Set to 1st of the month at 12:00 am
-            start: moment().startOf('month').add(1, 'days'),
-            // Set to end the 1st of the month at 3:00 am
-            end: moment().startOf('month').add({
-                'days': 1,
-                'hours': 3
-            }),
-            color: 'green'
-        }, {
-            title: 'Multi-day event',
-            // Set to start the 1st of the month
-            start: moment().startOf('month'),
-            // Set to end one week after the start of the month
-            end: moment().startOf('month').add(1, 'weeks'),
-            color: 'blue',
-            // This is an all-day event
-            allDay: true
-        }
+// eventsArray = [ {
+//         title: 'Event 1',
+//         // Set to 1st of the month at 12:00 am
+//         start: moment().startOf('month'),
+//         // Set to en the 1st of the month at 1:30 am
+//         end: moment().startOf('month').add(90, 'minutes')
+//         // color: 'red'
+//     }, {
+//         title: 'Event 2',
+//         // Set to 1st of the month at 12:00 am
+//         start: moment().startOf('month').add(1, 'days'),
+//         // Set to end the 1st of the month at 3:00 am
+//         end: moment().startOf('month').add({
+//             'days': 1,
+//             'hours': 3
+//         })
+//         // color: 'green'
+//     }, {
+//         title: 'Multi-day event',
+//         // Set to start the 1st of the month
+//         start: moment().startOf('month'),
+//         // Set to end one week after the start of the month
+//         end: moment().startOf('month').add(1, 'weeks')
+//         // color: 'blue',
+//         // // This is an all-day event
+//         // allDay: true
+//     }
 
-    ];
+// ];
 
-    var calendar = $('#calendar').fullCalendar({
-            // Start of calendar options
+// localStorage.setItem("events", JSON.stringify(eventsArray));
 
-            weekends: false,
+// if (formArr.find(x => x.value !== '')) {
+//     const todoToAdd = {
+//         id: database.todos.length,
+//         // id: localStorage.setItem("todosId", "todos".length),
+//         title: formArr.find(x => x.name === 'todo-title').value ? formArr.find(x => x.name === 'todo-title').value : 'No Title Set',
+//         description: formArr.find(x => x.name === 'todo-desc').value,
+//         dueDate: formArr.find(x => x.name === 'todo-date').value,
+//         status: formArr.find(x => x.name === 'todo-done') ? formArr.find(x => x.name === 'todo-done').value : 'off'
+//     };
 
-            header: {
-                left: 'today,month,agendaDay,agendaWeek',
-                center: 'title',
-                right: 'prev,next'
-            },
 
-            // Sets first day of week to Monday
-            firstDay: 1,
+//     const listTodoController = listTodos(database);
 
-            aspectRatio: 2.2,
+//     database.todos.push(todoToAdd);
+//     localStorage.setItem("todos", JSON.stringify(database.todos));
 
-            // Make possible to respond to clicks and selections
-            selectable: true,
+// localStorage.removeItem("localStorageEvents")
 
-            events: events_array,
+var parsedEvents;
+if (JSON.parse(localStorage.getItem("localStorageEvents")) === null) {
+    // parsedEvents = [{
+    //     title: 'No title',
+    //     start: moment().startOf('month'),
+    //     end: moment().startOf('month').add(90, 'minutes')
+    // }];
+} else {
+    // 
+    parsedEvents = JSON.parse(localStorage.getItem("localStorageEvents"));
+    console.log("parsedEvents= " + parsedEvents);
+}
+console.log([JSON.parse(localStorage.getItem("localStorageEvents"))]);
 
-            select: function(start, end, jsEvent, view) {
-                alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
-            },
+var calendar = $('#calendar').fullCalendar({
+        // Start of calendar options
 
-            // This is the callback that will be triggered when a selection is made.
-            // It gets start and end date/time as part of its arguments
-            select: function(start, end, jsEvent, view) {
+        height: 'auto',
+        
+        weekends: false,
 
-                // Ask for a title. If empty it will default to 'New event'
+        header: {
+            left: 'today,month,agendaDay,agendaWeek',
+            center: 'title',
+            right: 'prev,next'
+        },
 
-                var title = prompt('Enter a title for this event', 'New event');
+        // Sets first day of week to Monday
+        firstDay: 1,
 
-                // If did not pressed Cancel button
-                if (title != null) {
-                    // Create event
-                    var event = {
-                        title: title.trim() !== '' ? title : 'New event',
-                        start: start,
-                        end: end
-                    };
+        aspectRatio: 2.2,
 
-                    // Push event into fullCalendar's array of events
-                    // and displays it. The last argument is the
-                    // 'stick' value. If set to true the event
-                    // will 'stick' even after you move to other
-                    // year, month, day or week.
+        // Make possible to respond to clicks and selections
+        selectable: true,
 
-                    $calendar.fullCalendar('renderEvent', event, true);
+        events: JSON.parse(localStorage.getItem("localStorageEvents")),
+        // events: eventsArray,
+
+        // select: function(start, end, jsEvent, view) {
+        //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
+        // },
+
+        // This is the callback that will be triggered when a selection is made.
+        // It gets start and end date/time as part of its arguments
+        select: function(start, end, jsEvent, view) {
+
+            // Ask for a title. If empty it will default to 'New event'
+
+            var title = prompt('Enter a title for this event', 'New event');
+
+            // If did not pressed Cancel button
+            if (title != null) {
+                // Create event
+                var event = {
+                    title: title.trim() !== '' ? title : 'New event',
+                    start: start,
+                    end: end
                 };
-                // Whatever happens, unselect selection
-                $calendar.fullCalendar('unselect');
 
-            }, // End select callback
+                // Push event into fullCalendar's array of events
+                // and displays it. The last argument is the
+                // 'stick' value. If set to true the event
+                // will 'stick' even after you move to other
+                // year, month, day or week.
 
-            // Make events editable, globally
-            editable: true,
+                addEvent(parsedEvents, event);
 
-            // Callback triggered when we click on an event
-            eventClick: function(event, jsEvent, view) {
-                // Ask for a title. If empty it will default to 'New event'
-                var newTitle = prompt('Enter a new title for this event', event.title);
+                calendar.fullCalendar('renderEvent', event, true);
 
-                // If did not pressed Cancel button
-                if (newTitle != null) {
-                    // Update event
-                    event.title = newTitle.trim() !== '' ? newTitle : event.title;
+            };
+            // Whatever happens, unselect selection
+            calendar.fullCalendar('unselect');
 
-                    // Call the 'updateEvent' method
-                    $calendar.fullCalendar('updateEvent', event);
+        }, // End select callback
 
-                }
-            } // End callback eventClick
+        // Make events editable, globally
+        editable: true,
 
-            // // This is the callback that will be triggered when a selection is made
-            // select: function(start, end, jsEvent, view) {
-            //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
-            // }
+        // Callback triggered when we click on an event
+        eventClick: function(event, jsEvent, view) {
+            // Ask for a title. If empty it will default to 'New event'
+            var newTitle = prompt('Enter a new title for this event', event.title);
 
-        } // End of calendar options
-    );
-});
+            // If did not pressed Cancel button
+            if (newTitle != null) {
+                // Update event
+                event.title = newTitle.trim() !== '' ? newTitle : event.title;
+
+                // Call the 'updateEvent' method
+                calendar.fullCalendar('updateEvent', event);
+
+            }
+        } // End callback eventClick
+
+        // // This is the callback that will be triggered when a selection is made
+        // select: function(start, end, jsEvent, view) {
+        //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
+        // }
+
+    } // End of calendar options
+);
+// });
