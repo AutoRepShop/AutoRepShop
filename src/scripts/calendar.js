2f2190b3ed85a0d858a8d0fea6b05f14f2342cc6
Unknown <ahristoff@abv.bg>
2018-02-27 16:00:53 +0200

    fixed dev not working after messy merge

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,155 +1,153 @@
 // Create the calendar when the document is ready
 $(document).ready(function() {
     var calendar = $('#calendar').fullCalendar({
-            // Start of calendar options
+        // Start of calendar options
 
-            // height: 'auto',
+        // height: 'auto',
 
-            weekends: false,
+        weekends: false,
 
-            height: window.innerHeight * .9,
+        height: window.innerHeight * .9,
 
-            header: {
-                left: 'today,month,agendaDay,agendaWeek',
-                center: 'title',
-                right: 'prev,next'
-            },
+        header: {
+            left: 'today,month,agendaDay,agendaWeek',
+            center: 'title',
+            right: 'prev,next'
+        },
 
-            // Sets first day of week to Monday
-            firstDay: 1,
+        // Sets first day of week to Monday
+        firstDay: 1,
 
-            // Make possible to respond to clicks and selections
-            selectable: true,
+        // Make possible to respond to clicks and selections
+        selectable: true,
 
-            events: getAllEvents(),
-            // events: eventsArray,
+        events: getAllEvents(),
+        // events: eventsArray,
 
-            // select: function(start, end, jsEvent, view) {
-            //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
-            // },
-
-
-            // This is the callback that will be triggered when a selection is made.
-            // It gets start and end date/time as part of its arguments
-            select: function(start, end, jsEvent, view) {
-                // Ask for a title. If empty it will default to 'New event'
-
-                var title = prompt('Enter a title for this event', 'New event');
-
-                // If did not pressed Cancel button
-                if (title != null) {
-                    // Create event
-                    var event = {
-                        id: getEventId(),
-                        title: title.trim() !== '' ? title : 'New event',
-                        start: start,
-                        end: end,
-                        color: 'red'
-                    };
-
-                    // Push event into fullCalendar's array of events
-                    // and displays it. The last argument is the
-                    // 'stick' value. If set to true the event
-                    // will 'stick' even after you move to other
-                    // year, month, day or week.
+        // select: function(start, end, jsEvent, view) {
+        //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
+        // },
 
 
-                    calendar.fullCalendar('renderEvent', event, true);
-                    addEvent(getAllEvents(), event);
+        // This is the callback that will be triggered when a selection is made.
+        // It gets start and end date/time as part of its arguments
+        select: function(start, end, jsEvent, view) {
+            // Ask for a title. If empty it will default to 'New event'
 
+            var title = prompt('Enter a title for this event', 'New event');
+
+            // If did not pressed Cancel button
+            if (title != null) {
+                // Create event
+                var event = {
+                    id: getEventId(),
+                    title: title.trim() !== '' ? title : 'New event',
+                    start: start,
+                    end: end,
+                    color: 'red'
                 };
 
-                // Whatever happens, unselect selection
-                calendar.fullCalendar('unselect');
+                // Push event into fullCalendar's array of events
+                // and displays it. The last argument is the
+                // 'stick' value. If set to true the event
+                // will 'stick' even after you move to other
+                // year, month, day or week.
 
-            }, // End select callback
 
-            // Make events editable, globally
-            editable: true,
+                calendar.fullCalendar('renderEvent', event, true);
+                addEvent(getAllEvents(), event);
 
-            // Callback triggered when we click on an event
-            eventClick: function(event, jsEvent, view) {
-                // Ask for a title. If empty it will default to 'New event'
-                var newTitle = prompt('Enter a new title for this event', event.title);
+            };
 
-            eventClick: function(event, element) {
-                // Display the modal and set the values to the event values.
-                $('#myModal').load('eventForm.html', function() {
-                    $('.modal').show();
-                });
+            // Whatever happens, unselect selection
+            calendar.fullCalendar('unselect');
 
-                // saveButton().on('click', function() {
-                //     $('#myModal').find('#title').val(event.title);
+        }, // End select callback
 
-            eventDrop: function(event, delta, revertFunc) {
-                if (!confirm(`${event.title + ' was dropped on ' + event.start.format()}
+        // Make events editable, globally
+        editable: true,
+
+        // Callback triggered when we click on an event
+        // eventClick: function(event, jsEvent, view) {
+        //     // Ask for a title. If empty it will default to 'New event'
+        //     var newTitle = prompt('Enter a new title for this event', event.title);
+
+        eventClick: function(event, element) {
+            // Display the modal and set the values to the event values.
+            $('#myModal').load('eventForm.html', function() {
+                $('.modal').show();
+            });
+        },
+        // saveButton().on('click', function() {
+        //     $('#myModal').find('#title').val(event.title);
+
+        eventDrop: function(event, delta, revertFunc) {
+            if (!confirm(`${event.title + ' was dropped on ' + event.start.format()}
                             \nAre you sure about this change?`)) {
-                    revertFunc();
-                    return;
-                }
+                revertFunc();
+                return;
+            }
 
-                const currentEvent = {
-                    id: event.id,
-                    title: event.title,
-                    start: event.start - delta,
-                    end: event.end - delta
-                };
+            const currentEvent = {
+                id: event.id,
+                title: event.title,
+                start: event.start - delta,
+                end: event.end - delta
+            };
 
-                updateEvent(currentEvent, event);
-            },
+            updateEvent(currentEvent, event);
+        },
 
-            eventResize: function(event, delta, revertFunc) {
-                if (!confirm(`${event.title + ' now ends on ' + event.end.format()}
+        eventResize: function(event, delta, revertFunc) {
+            if (!confirm(`${event.title + ' now ends on ' + event.end.format()}
                             \nIs this okay?`)) {
-                    revertFunc();
-                    return;
-                }
-
-                const currentEvent = {
-                    id: event.id,
-                    title: event.title,
-                    start: event.start - delta,
-                    end: event.end - delta
-                };
-
-                updateEvent(currentEvent, event);
+                revertFunc();
+                return;
             }
 
-                //     modal.style.display = "none";
-                // });
+            const currentEvent = {
+                id: event.id,
+                title: event.title,
+                start: event.start - delta,
+                end: event.end - delta
+            };
+
+            updateEvent(currentEvent, event);
+        }
+
+        //     modal.style.display = "none";
+        // });
 
 
 
-                // $('#myModal').modal('show');
-                // $('#myModal').find('#starts-at').val(event.start);
-                // $('#myModal').find('#ends-at').val(event.end);
+        // $('#myModal').modal('show');
+        // $('#myModal').find('#starts-at').val(event.start);
+        // $('#myModal').find('#ends-at').val(event.end);
 
-            }
+    });
 
-            // // Callback triggered when we click on an event
-            // eventClick: function(event, jsEvent, view) {
-            //         // Ask for a title. If empty it will default to 'New event'
-            //         var newTitle = prompt('Enter a new title for this event', event.title);
+    // // Callback triggered when we click on an event
+    // eventClick: function(event, jsEvent, view) {
+    //         // Ask for a title. If empty it will default to 'New event'
+    //         var newTitle = prompt('Enter a new title for this event', event.title);
 
-            //     // If did not pressed Cancel button
-            //     if (newTitle != null) {
-            //         let currentEvent = event;
-            //         // Update event
-            //         event.title = newTitle.trim() !== '' ? newTitle : event.title;
+    //     // If did not pressed Cancel button
+    //     if (newTitle != null) {
+    //         let currentEvent = event;
+    //         // Update event
+    //         event.title = newTitle.trim() !== '' ? newTitle : event.title;
 
-            //         //update localStorage
-            //         updateEvent(currentEvent, event);
-            //         // Call the 'updateEvent' method
-            //         calendar.fullCalendar('updateEvent', event);
+    //         //update localStorage
+    //         updateEvent(currentEvent, event);
+    //         // Call the 'updateEvent' method
+    //         calendar.fullCalendar('updateEvent', event);
 
-            //         }
-            //     } // End callback eventClick
+    //         }
+    //     } // End callback eventClick
 
-            // // This is the callback that will be triggered when a selection is made
-            // select: function(start, end, jsEvent, view) {
-            //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
-            // }
+    // // This is the callback that will be triggered when a selection is made
+    // select: function(start, end, jsEvent, view) {
+    //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
+    // }
 
-        } // End of calendar options
-    );
 });
