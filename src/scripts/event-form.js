653a12685f64fe71c2b29f5e6b07491776625172
IovoslavIovchev <iovoslav.i@gmail.com>
2018-03-01 09:49:27 +0200

    Pre-final

--- src/scripts/event-form.js
+++ src/scripts/event-form.js
@@ -1,6 +1,47 @@
+// function saveEventModalInfo(event, typeOfAction) {
+//     // Get the modal
+//     var modal = document.getElementById('myModal');
+
+//     document.getElementById('save-event').addEventListener('click', function() {
+//         //save info in localStorage
+//         event.title = $('#title').val();
+//         event.start = $('#starts-at').val();
+//         event.end = $('#ends-at').val();
+//         event.clientName = $('#clientName').val();
+//         event.tel = $('#telephone').val();
+//         event.vehicle = $('#vehicle').val();
+//         event.description = $('#description').val();
+
+//         if (validate() === 'problem') {
+//             return;
+//         }
+
+//         if (validate() !== 'problem') {
+//             modal.style.display = 'none';
+//             //location.reload();
+//         }
+
+//         if (typeOfAction === 'new') {
+//             localStorage.setItem('nextEventId', JSON.stringify(parseInt(event.id) + 1));
+//             eventHandler.addEvent(eventHandler.getAllEvents(), event);
+//         } else if (typeOfAction === 'edit') {
+//             // update events in localStorage
+//             updateEvent(event);
+//         }
+
+//         if (validate() !== 'problem') {
+//             // debugger;
+//             modal.style.display = 'none';
+//             alert(`Your event PIN is ${validationsHandler.getEventPin(event.id)}  \n Please, remember it! \n It's required for any change of appointment \n and if you want to leave feedback `);
+//             location.reload();
+//         }
+//     });
+// };
+
 function saveEventModalInfo(event, typeOfAction) {
     // Get the modal
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementsByClassName('modal')[0];
+    var span = document.getElementsByClassName('close')[0];
 
     document.getElementById('save-event').addEventListener('click', function() {
         //save info in localStorage
@@ -12,28 +53,41 @@ function saveEventModalInfo(event, typeOfAction) {
         event.vehicle = $('#vehicle').val();
         event.description = $('#description').val();
 
+        debugger;
         if (validate() === 'problem') {
             return;
         }
 
-        if (validate() !== 'problem') {
-            modal.style.display = 'none';
-            //location.reload();
-        }
-
         if (typeOfAction === 'new') {
+            if (validate() !== 'problem') {
+                modal.style.display = 'none';
+                $('#myModal').hide();
+            };
+
+            //store the next available id in LS
             localStorage.setItem('nextEventId', JSON.stringify(parseInt(event.id) + 1));
             eventHandler.addEvent(eventHandler.getAllEvents(), event);
-        } else if (typeOfAction === 'edit') {
-            // update events in localStorage
-            updateEvent(event);
-        }
 
-        if (validate() !== 'problem') {
-            // debugger;
-            modal.style.display = 'none';
-            alert(`Your event PIN is ${validationsHandler.getEventPin(event.id)}  \n Please, remember it! \n It's required for any change of appointment \n and if you want to leave feedback `);
+            alert(`Your registration PIN is ${validationsHandler.getEventPin(event.id)}  
+            \n Please, remember it! 
+            \n It's required for any change of appointment 
+            \n and if you want to leave feedback `);
+            location.reload();
+
+        } else if (typeOfAction === 'edit') {
+            if (validate() !== 'problem') {
+                // update events in localStorage
+                eventHandler.updateEvent(event);
+                //close the modal
+                modal.style.display = 'none';
+                $('#myModal').hide();
+            };
             location.reload();
         }
+    });
+
+    span.addEventListener('click', function() {
+        modal.style.display = 'none';
+        $('#myModal').hide();
     });
 };
