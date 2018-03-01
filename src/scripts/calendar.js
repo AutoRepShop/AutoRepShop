e76ae5eb0a7fa4abfc9ef0719527b2a9724fa5dc
IovoslavIovchev <iovoslav.i@gmail.com>
2018-03-01 09:01:55 +0200

    Starting the refactoring

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,20 +1,16 @@
 // Create the calendar when the document is ready
 // $(document).ready(function() {
 var calendar = $('#calendar').fullCalendar({
-    // Start of calendar options
-
-    // height: 'auto',
-
     weekends: false,
 
     businessHours: {
         // days of week. an array of zero-based day of week integers (0=Sunday)
         dow: [1, 2, 3, 4, 5], // Monday - Thursday
-        start: '08:00', // a start time (10am in this example)
-        end: '20:00' // an end time (6pm in this example)
+        start: '08:00',
+        end: '20:00'
     },
 
-    height: window.innerHeight * .9,
+    height: window.innerHeight * .85,
 
     header: {
         left: 'month,agendaDay,agendaWeek',
@@ -29,15 +25,10 @@ var calendar = $('#calendar').fullCalendar({
     selectable: true,
 
     events: getAllEvents(),
-    // events: eventsArray,
-
-    // select: function(start, end, jsEvent, view) {
-    //     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
-    // },
 
     // This is the callback that will be triggered when a selection is made.
     // It gets start and end date/time as part of its arguments
-    select: function(start, end, jsEvent, view) {
+    select: function(start, end) {
         // Create event
         var event = {
             id: getEventId(),
@@ -55,52 +46,12 @@ var calendar = $('#calendar').fullCalendar({
             $('.modal').show();
             saveEventModalInfo(event, 'new');
         });
-
-
-        // Ask for a title. If empty it will default to 'New event'
-        // var title = prompt('Enter a title for this event', 'New event');
-
-        // If did not pressed Cancel button
-        // if (title != null) {
-
     },
-
-
-    // Push event into fullCalendar's array of events
-    // and displays it. The last argument is the
-    // 'stick' value. If set to true the event
-    // will 'stick' even after you move to other
-    // year, month, day or week.
-
-    // calendar.fullCalendar('renderEvent', event, true);
-
-
-    //LEAVE LIKE THAT FOR NOW
-    // eventClick: function(event, element) {
-    //     debugger;
-    //     // Display the modal and set the values to the event values.
-    //     if (validatePin(event.id)) {
-    //         $('#myModal').load('eventForm.html', function() {
-    //             $('.modal').show();
-    //         });
-
-    //         // };
-
-    //         // Whatever happens, unselect selection
-    //         calendar.fullCalendar('unselect');
-    //         // updateEvent(currentEvent, event);
-    //     }
-
-    // }, // End select callback
 
     // Make events editable, globally
     editable: true,
 
-    // // Callback triggered when we click on an event
-    // eventClick: function(event, jsEvent, view) {
-    //     // Ask for a title. If empty it will default to 'New event'
-    //     var newTitle = prompt('Enter a new title for this event', event.title);
-
+    // Defines what happens when you click on an existing event
     eventClick: function(event, element) {
         // Display the modal and set the values to the event values.
         if (validatePin(event.id)) {
@@ -119,14 +70,10 @@ var calendar = $('#calendar').fullCalendar({
                 $('#eventModalId').text('Edit Appointment');
                 saveEventModalInfo(event, 'edit');
             });
-
-            // updateEvent(event);
-
         }
     },
-    // saveButton().on('click', function() {
-    //     $('#myModal').find('#title').val(event.title);
 
+    // Defines what happens when you drag and drop an existing event
     eventDrop: function(event, delta, revertFunc) {
         if (validatePin(event.id)) {
             if (!confirm(`${event.title + ' was dropped on ' + event.start.format()}
@@ -141,51 +88,4 @@ var calendar = $('#calendar').fullCalendar({
 
         revertFunc();
     }
-
-    // OBSOLETE ?
-    // eventResize: function(event, delta, revertFunc) {
-    //     if (!confirm(`${event.title + ' now ends on ' + event.end.format()}
-    //                         \nIs this okay?`)) {
-    //         revertFunc();
-    //         return;
-    //     }
-
-    //     updateEvent(event);
-    // }
-
-    //     modal.style.display = 'none';
-    // });
-
-
-
-    // $('#myModal').modal('show');
-    // $('#myModal').find('#starts-at').val(event.start);
-    // $('#myModal').find('#ends-at').val(event.end);
-
 });
-
-// // Callback triggered when we click on an event
-// eventClick: function(event, jsEvent, view) {
-//         // Ask for a title. If empty it will default to 'New event'
-//         var newTitle = prompt('Enter a new title for this event', event.title);
-
-//     // If did not pressed Cancel button
-//     if (newTitle != null) {
-//         let currentEvent = event;
-//         // Update event
-//         event.title = newTitle.trim() !== '' ? newTitle : event.title;
-
-//         //update localStorage
-//         updateEvent(currentEvent, event);
-//         // Call the 'updateEvent' method
-//         calendar.fullCalendar('updateEvent', event);
-
-//         }
-//     } // End callback eventClick
-
-// // This is the callback that will be triggered when a selection is made
-// select: function(start, end, jsEvent, view) {
-//     alert(start.format('MM/DD/YYYY hh:mm a') + ' to ' + end.format('MM/DD/YYYY h\h:mm a') + ' in view ' + view.name);
-// }
-
-// });
